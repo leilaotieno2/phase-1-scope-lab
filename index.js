bb8ccfa0b4c6b122abd708260e42aac4f51db97f
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-06 20:15:07 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+let customerName = "bob";
+
+function returnCustomerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+let bestCustomer = "initial best customer";
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "initial least favorite customer";
+
+function attemptLeastFavoriteCustomerChange() {
+  leastFavoriteCustomer = "new least favorite customer";
+  // This will result in an error because const variables cannot be reassigned
+}
